de16ebb00499c110f32994b30937ab1ee209c6c6
Mariano <mariano.martinotti@pampago.site>
2026-04-26 14:04:22 -0300

    add && now

--- api/config/cors.php
+++ api/config/cors.php
@@ -1,6 +1,7 @@
 <?php
 
-header("Access-Control-Allow-Origin: http://localhost:5173");
+//header("Access-Control-Allow-Origin: http://localhost:5173");
+header("Access-Control-Allow-Origin: pampamind.pampago.site");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 header("Content-Type: application/json");
